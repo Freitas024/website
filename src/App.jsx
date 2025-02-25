78d1994fbd9f2a4f8a2f49e097f84d3d31b6b3b0
Freitas024 <viniciusfreitassilva1999@gmail.com>
2025-02-24 23:47:58 -0300

    refactor: remodelagem do site

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,23 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { GlobalStyles, lightTheme, darkTheme } from "./styles/GlobalStyles";
-import Home from "./pages/Home/index";
+import Home from "./pages/Home";
+import Works from "./pages/works";
 import { ThemeProvider } from "styled-components";
-import { List, ButtonToggle } from './styles/App';
-import { Button } from './styles/Button';
-import { icons } from "./settings";
 
 export default function App() {
 
   const [isDarkTheme, setIsDarkTheme] = useState(true);
   const [toggle, setToggle ] = useState(false);
 
-  const handleToggle = () => {
-    setToggle(!toggle);
-  }
-
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <GlobalStyles/>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/works" element={<Works />} />
         </Routes>
-        <Button onClick={handleToggle}>
-        <img src={icons.Gear}/>
-        </Button>
-          {toggle && (
-            <List>
-              <h4>Tema</h4>
-              <ButtonToggle onClick={() => {setIsDarkTheme(true)}}>
-                <img src={icons.Moon}/>
-                Dark
-              </ButtonToggle>
-              <ButtonToggle onClick={() => {setIsDarkTheme(false)}}> 
-                <img src={icons.Sun}/>
-                Light 
-              </ButtonToggle>
-              <h4>Redes Sociais</h4>
-              <a href="https://github.com/Freitas024" target="_blank">
-                <ButtonToggle>
-                  <img src={icons.Github}/>
-                  Github
-                </ButtonToggle>
-              </a>
-              <a href="https://www.linkedin.com/in/vinicius-de-freitas-e-silva-137a02295/" target="_blank">
-                <ButtonToggle>
-                  <img src={icons.Linkedin}/>
-                  Linkedin
-                </ButtonToggle>
-              </a>
-              <a href="https://www.instagram.com/ux_vini/profilecard/?igsh=MXB4eHZuNmd0NHR1dA==" target="_blank">
-                <ButtonToggle>
-                  <img src={icons.Instagram}/>
-                  Instagram
-                </ButtonToggle>
-              </a>
-            </List>
-          )}
       </Router>
     </ThemeProvider>
   );
---
+++ src/pages/works/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Works() {
+    return(
+        <main>
+            <h3>Projetos do meu Github.</h3>
+            <h4>Em breve ....</h4>
+        </main>
+    )
+}
