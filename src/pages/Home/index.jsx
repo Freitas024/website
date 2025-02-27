79355e69666d664a71318ecba09064d118b0fa83
Freitas024 <viniciusfreitassilva1999@gmail.com>
2025-02-27 13:35:29 -0300

    chore: recriando pagina home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { HomeContainer, ProfileContainer } from "./styles";
 
 export default function Home() {
+
+    const [user, setUser] = useState("");
+    
+    useEffect(() => { 
+        fetch("https://api.github.com/users/Freitas024")
+        .then(response => response.json())
+        .then(data => setUser(data.avatar_url))
+        .catch(error => console.log("Erro: " + error));
+
+    }, []);
+
     return(
         <HomeContainer>
             <ProfileContainer>
-                <h3>Vinicius de Freitas e Silva</h3>
-                <p>Desenvolvedor Front-end | UX/UI Designer</p>
+                <img src={user} alt="Vinicius de Freitas e Silva" />
+                <div>
+                    <h3>Vinicius de Freitas e Silva</h3>
+                    <p>Desenvolvedor Front-end | UX/UI Designer</p>
+                </div>
             </ProfileContainer>
         </HomeContainer>
     )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -10,5 +10,25 @@ export const ProfileContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    flex-direction: column;
+    flex-direction: row;
+    margin: 60px 0px;
+
+    img {
+        width: 80px;
+        height: 80px;
+        margin: 0px 30px;
+        border-radius: 5%;
+    }
+
+     div {
+        height: 50px;
+        display: flex;
+        align-items: start;
+        justify-content: space-around;
+        flex-direction: column;
+
+        p {
+            color: #7543fe;
+        }
+     }
 `;
